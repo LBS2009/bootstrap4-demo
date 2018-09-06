ae9dacd04c3ca47d926642a3b138a7ebd67d3a75
libosheng <libosheng@news.cn>
2018-09-06 16:58:10 +0800

    Initial commit

---
+++ src/main/java/com/lbs/bootstrap4demo/Bootstrap4DemoApplication.java
@@ -0,0 +1,12 @@
+package com.lbs.bootstrap4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap4DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstrap4DemoApplication.class, args);
+	}
+}
